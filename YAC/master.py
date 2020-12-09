c1860a9e8b8e520a8692736ac7c5552bd01332a8
hat-im <hatim3121@gmail.com>
2020-12-09 14:52:12 +0530

    updated things, just smoothed things out

--- YAC/master.py
+++ YAC/master.py
@@ -11,14 +11,14 @@ from datetime import datetime
 from collections import defaultdict
 
 global availableSlots
-global taskQueue
-global mapTaskCounts
-global taskQueue
-global reduceTasks
-global jobTaskCounts
+global tasks_queue
+global map_tasks_count
+global tasks_queue
+global red_tasks
+global job_tasks_count
 global availableSlots
-global mapTaskCounts
-global jobTaskCounts
+global map_tasks_count
+global job_tasks_count
 
 
 
@@ -28,214 +28,268 @@ class myThread(threading.Thread):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.func = func
-    
     def run(self):
         self.func()
 
 def RANDOM():
-    for i in iter(int,1):
+    while 1:
         worker_id = random.randint(1, 3)
         test_condition = availableSlots[worker_id]['slots']
-        if  test_condition < 0:
-                pass
+        if  test_condition <= 0:
+                continue
         else:
                 return worker_id
 
 def RR():
-    for i in iter(int,1):
+    while 1:
         for worker_id in range(1,4):
             test_condition = availableSlots[worker_id]['slots']
-            if  test_condition < 0:
-                pass
+            if  test_condition <= 0:
+                continue
             else:
                 return worker_id
 
 def LL():
-    while:
-        x= max(range(1,4),key=lambda x: availableSlots[x]['slots'])
-        if x!=None:
-            return x
+    while 1:
+        test_condition = max(range(1,4),key=lambda x: availableSlots[x]['slots'])
+        if test_condition:
+            return test_condition
 
-functions = {"RANDOM":RANDOM,"RR":RR,"LL":LL}
+
 
 #allocate tasks to the right worker     
-def scheduler(): 
+def scheduler():
+    functions = {"RANDOM":RANDOM,"RR":RR,"LL":LL}
     scheduling_algo = sys.argv[2]
     for i in iter(int,1):
-        taskQueueSize = len(taskQueue)
+        taskQueueSize = len(tasks_queue)
         if(taskQueueSize < 0):
-            pass
-        else:
-            for task in taskQueue:
+            return
+        for task in tasks_queue:
+            try:
+                worker_id = functions[scheduling_algo]()
+            except:
+                print("Invalid Input")
+                return
+            port = availableSlots[worker_id]['port']
+            task = tasks_queue.pop(0)
+            task_message = json.dumps(task)
+            try: 
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                s.connect(("localhost", port))
                 try:
-                     worker_id = functions[scheduling_algo]()
-                except:
-                    print("Invalid Input")
-                    return
-                
-                #picking the correct worker 
-                port = availableSlots[worker_id]['port']
-                task = taskQueue[0]
-                taskQueue.remove(task)
-                task_message = json.dumps(task)
-                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                    s.connect(("localhost", port))
-                
                     f = open("log/master.txt", "a+")
-                    #with open("log/master.txt", "a+") as f:
-                    try:
-                        printLock.acquire()
-                        print(datetime.now().strftime("%m/%d/%Y, %H:%M:%S.%f") + f"\tCONNECTION OUTGOING = [host:{'localhost'}, port:{port}]",file=f)
-                        printLock.release()
-                    finally:
-                        f.close()
-
-                    s.send(task_message.encode())
-            
-
-
-                
-                f = open("log/master.txt", "a+")
-                try:
-                    printLock.acquire()
-                    print(datetime.now().strftime("%m/%d/%Y, %H:%M:%S.%f") + f"\tTASK SENT TO = [worker_id:{worker_id}, task_id:{task['task_id']}, job_id:{task['job_id']}, duration:{task['duration']}]",file=f)
-                    printLock.release()
-                finally:
+                    log_lock.acquire()
+                    print(datetime.now().strftime("%m/%d/%Y, %H:%M:%S.%f") + f"\tCONNECTION OUTGOING = [host:{'localhost'}, port:{port}]",file=f)
+                    log_lock.release()
+                except:
+                    print("UNKNOWN ERROR OCCURED DURING LOGGING")
+                else:
                     f.close()
+                s.send(task_message.encode())
+            except Exception as e:
+                try:
+                    f = open("log/master.txt", "a+")
+                    log_lock.acquire()
+                    print(datetime.now().strftime("%m/%d/%Y, %H:%M:%S.%f") + f"\tERROR OCCURED [error_message: {e}]",file=f)
+                    log_lock.release()
+                except:
+                    print("UNKNOWN ERROR OCCURED DURING LOGGING")
+                else:
+                    f.close()
+            try:
+                f = open("log/master.txt", "a+")
+                log_lock.acquire()
+                print(datetime.now().strftime("%m/%d/%Y, %H:%M:%S.%f") + f"\tTASK SENT TO = [worker_id:{worker_id}, task_id:{task['task_id']}, job_id:{task['job_id']}, duration:{task['duration']}]",file=f)
+                log_lock.release()
+            except Exception as e:
+                print(f"UNKNOWN ERROR OCCURED DURING LOGGING = [error_message:{e}]")
+            else:
+                f.close()
+            print(f"Sent task {task['task_id']} to worker {worker_id} on port {port}")
+            worker_lock.acquire()
+            availableSlots[worker_id]['slots'] -= 1
+            worker_lock.release()
 
-                print(f"Sent task {task['task_id']} to worker {worker_id} on port {port}")
-                
-                workerLock.acquire()
-                availableSlots[worker_id]['slots'] -= 1
-                workerLock.release()
-    
-       
-#listen for requests
+
 def getRequests():
     host = ""
     port = 5000
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    
-    with s:
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.bind((host, port))
         s.listen(1000)
         while(True):
-            c, addr = s.accept()
-            with open("log/master.txt", "a+") as f:
-                printLock.acquire()
+            conn, addr = s.accept()
+            try:
+                f = open("log/master.txt", "a+")
+                log_lock.acquire()
                 print(datetime.now().strftime("%m/%d/%Y, %H:%M:%S.%f") + f"\tCONNECTION INCOMING = [host:{addr[0]}, port:{addr[1]}]",file=f)
-                printLock.release()
-            with c:
-                req = c.recv(100000)
-                req = json.loads(req)
-                task_list={"map":[m_t['task_id'] for m_t in req['map_tasks']],"reduce":[r_t['task_id'] for r_t in req['reduce_tasks']]}
-                with open("log/master.txt", "a+") as f:
-                    printLock.acquire()
-                    print(datetime.now().strftime("%m/%d/%Y, %H:%M:%S.%f") + f"\tRECEIVED JOB REQUEST = [job_id:{req['job_id']}, map_tasks_ids:{task_list['map']}, reduce_tasks_ids:{task_list['reduce']}]",file=f)
-                    printLock.release()
-                no_map_tasks = len(req['map_tasks'])
-                mapTaskCounts[req['job_id']] = no_map_tasks
-                jobTaskCounts[req['job_id']] = len(req['map_tasks']) + len(req['reduce_tasks'])
-                reduceTasks[req['job_id']] = []
-                tasks = req['reduce_tasks']
-                task = {}
-                for i in tasks:
-                    task[i['task_id']] = {}
-                    task[i['task_id']]['task_id'] = i['task_id'] 
-                    task[i['task_id']]['job_id'] = req['job_id']
-                    task[i['task_id']]['duration'] = i['duration']
-                    reduceTasks[req['job_id']].append(task[i['task_id']])
-                    
+                log_lock.release()
+            except Exception as e:
+                print(f"UNKNOWN ERROR OCCURED DURING LOGGING = [error_message:{e}]")
+            else:
+                f.close()
+            req = json.loads(conn.recv(100000))
+            task_list={"map":[m_t['task_id'] for m_t in req['map_tasks']],"reduce":[r_t['task_id'] for r_t in req['reduce_tasks']]}
+            try: 
+                f = open("log/master.txt", "a+")
+                log_lock.acquire()
+                print(datetime.now().strftime("%m/%d/%Y, %H:%M:%S.%f") + f"\tRECEIVED JOB REQUEST = [job_id:{req['job_id']}, map_tasks_ids:{task_list['map']}, reduce_tasks_ids:{task_list['reduce']}]",file=f)
+                log_lock.release()
+            except Exception as e:
+                print(f"UNKNOWN ERROR OCCURED DURING LOGGING = [error_message:{e}]")
+            else:
+                f.close()
+            no_map_tasks = len(req['map_tasks'])
+            map_tasks_count[req['job_id']] = no_map_tasks
+            job_tasks_count[req['job_id']] = len(req['map_tasks']) + len(req['reduce_tasks'])
+            red_tasks[req['job_id']] = list()
+            task = dict()
+            for i in req['reduce_tasks']:
+                task[i['task_id']] = dict()
+                task[i['task_id']]['task_id'] = i['task_id'] 
+                task[i['task_id']]['job_id'] = req['job_id']
+                task[i['task_id']]['duration'] = i['duration']
+                red_tasks[req['job_id']].append(task[i['task_id']])
+            task = dict()
+            for i in req['map_tasks']:
+                task[i['task_id']] = dict()
+                task[i['task_id']]['task_id'] = i['task_id'] 
+                task[i['task_id']]['job_id'] = req['job_id']
+                task[i['task_id']]['duration'] = i['duration']
+                tasks_queue.append(task[i['task_id']])
+    except Exception as e:
+        try: 
+            f = open("log/master.txt", "a+")
+            log_lock.acquire()
+            print(datetime.now().strftime("%m/%d/%Y, %H:%M:%S.%f") + f"\tERROR OCCURED [error_message: {e}]",file=f)
+            log_lock.release()
+        except Exception as e:
+            print(f"UNKNOWN ERROR OCCURED DURING LOGGING = [error_message:{e}]")
+        else:
+            f.close()
+    else:
+        s.close()
 
-
-                tasks = req['map_tasks']
-                task = {}
-                for i in tasks:
-                    task[i['task_id']] = {}
-                    task[i['task_id']]['task_id'] = i['task_id'] 
-                    task[i['task_id']]['job_id'] = req['job_id']
-                    task[i['task_id']]['duration'] = i['duration']
-                    taskQueue.append(task[i['task_id']])
-                
 
 def getUpdatesWorkers():
-    
     host = ""
     port = 5001
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    with s:
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.bind((host, port))
         s.listen(1000)
         while(True):
-            c, addr = s.accept()
-            with open("log/master.txt", "a+") as f:
-                printLock.acquire()
-                print(datetime.now().strftime("%m/%d/%Y, %H:%M:%S.%f") + f"\tCONNECTION INCOMING = [host:{addr[0]}, port:{addr[1]}]",file=f)
-                printLock.release()
-            with c:
-                update_json = c.recv(1000)
+            try:
+                conn, addr = s.accept()
+                try:
+                    f = open("log/master.txt", "a+")
+                    log_lock.acquire()
+                    print(datetime.now().strftime("%m/%d/%Y, %H:%M:%S.%f") + f"\tCONNECTION INCOMING = [host:{addr[0]}, port:{addr[1]}]",file=f)
+                    log_lock.release()
+                except Exception as e:
+                    print(f"UNKNOWN ERROR OCCURED DURING LOGGING = [error_message:{e}]")
+                else:
+                    f.close()
+                update_json = conn.recv(1000)
                 update = json.loads(update_json)
                 worker_id = update['worker_id']
                 job_id = update['job_id']
                 task_id = update['task_id']
-                with open("log/master.txt", "a+") as f:
-                    printLock.acquire()
-                    print(datetime.now().strftime("%m/%d/%Y, %H:%M:%S.%f") + f"\t TASK COMPLETED AT = [worker_id:{worker_id}, task_id:{task_id}]",file=f)
-                    printLock.release()
+                try:
+                    f = open("log/master.txt", "a+")
+                    log_lock.acquire()
+                    print(datetime.now().strftime("%m/%d/%Y, %H:%M:%S.%f") + f"\tTASK COMPLETED AT = [worker_id:{worker_id}, task_id:{task_id}]",file=f)
+                    log_lock.release()
+                except Exception as e:
+                    print(f"UNKNOWN ERROR OCCURED DURING LOGGING = [error_message:{e}]")
+                else:
+                    f.close()
                 print(f"Task {task_id} completed execution in {worker_id}")
-                jobTaskCounts[job_id] -= 1
-                if jobTaskCounts[job_id] == 0:
-                    with open("log/master.txt", "a+") as f:
-                        printLock.acquire()
+                job_tasks_count[job_id] -= 1
+                if job_tasks_count[job_id] == 0:
+                    try:
+                        f = open("log/master.txt", "a+")
+                        log_lock.acquire()
                         print(datetime.now().strftime("%m/%d/%Y, %H:%M:%S.%f") + f"\tFINISHED EXECUTING JOB = [job_id:{job_id}]",file=f)
-                        printLock.release()
-                workerLock.acquire()
+                        log_lock.release()
+                    except Exception as e:
+                        print(f"UNKNOWN ERROR OCCURED DURING LOGGING = [error_message:{e}]")
+                    else:
+                        f.close()
+                worker_lock.acquire()
                 availableSlots[worker_id]['slots'] += 1
-                workerLock.release()
-                
-                if 'M' in task_id: #map job is completed
-                    mapTaskCounts[job_id] -= 1
-                    if mapTaskCounts[job_id] == 0:
-                        for reduce_task in reduceTasks[job_id]:
-                            taskQueue.append(reduce_task)
-                        
-                    
+                worker_lock.release()
+                if 'M' in task_id:
+                    map_tasks_count[job_id] -= 1
+                    if map_tasks_count[job_id] == 0:
+                        for reduce_task in red_tasks[job_id]:
+                            tasks_queue.append(reduce_task)
+            except Exception as e:
+                try: 
+                    f = open("log/master.txt", "a+")
+                    log_lock.acquire()
+                    print(datetime.now().strftime("%m/%d/%Y, %H:%M:%S.%f") + f"\tERROR OCCURED [error_message: {e}]",file=f)
+                    log_lock.release()
+                except Exception as e:
+                    print(f"UNKNOWN ERROR OCCURED DURING LOGGING = [error_message:{e}]")
+                else:
+                    f.close()
+            else:
+                conn.close()
+    except Exception as e:
+        try: 
+            f = open("log/master.txt", "a+")
+            log_lock.acquire()
+            print(datetime.now().strftime("%m/%d/%Y, %H:%M:%S.%f") + f"\tERROR OCCURED [error_message: {e}]",file=f)
+            log_lock.release()
+        except Exception as e:
+            print(f"UNKNOWN ERROR OCCURED DURING LOGGING = [error_message:{e}]")
+        else:
+            f.close()
+    else:
+        s.close()
 
-mapTaskCounts = {} #number of map tasks of every job remaining to be executed
-jobTaskCounts = {} #total number of tasks in every job remaining to be executed
-reduceTasks = {} #list of reduce tasks for every job
-taskQueue = [] #list of tasks to be scheduled
-workerLock = threading.Lock()
+
+map_tasks_count = dict()
+job_tasks_count = dict()
+red_tasks = dict()
+tasks_queue = list()
+worker_lock = threading.Lock()
 taskLock = threading.Lock()
-printLock = threading.Lock()
+log_lock = threading.Lock()
 
 
-#make a new dir for the logs
-try:
+if not(os.path.isdir('log')):
     os.mkdir('log')
-#Skip if already present
-except:
-    pass 
 
 open("log/master.txt", "w").close()
 
-path_to_config = sys.argv[1]
-with open(path_to_config) as f:
-    config = json.load(f)
-#print(config)
 
-availableSlots = {} 
+try:
+    f = open(sys.argv[1])
+    config = json.load(f)
+except:
+    pass
+else:
+    f.close()
+
+if verbose:
+    print(config)
+
+availableSlots = dict()
 for worker in config['workers']:
     availableSlots[worker['worker_id']] = {'slots':worker['slots'], 'port':worker['port']}
 
 print(availableSlots)
-thread1 = myThread(1, getRequests)
-thread2 = myThread(2, getUpdatesWorkers)
-thread3 = myThread(3, scheduler)
-thread1.start()
-thread2.start()
-thread3.start()
-thread1.join()
-thread2.join()
-thread3.join()
+
+request_thread = myThread(1, getRequests)
+workers_thread = myThread(2, getUpdatesWorkers)
+schedule_thread = myThread(3, scheduler)
+request_thread.start()
+workers_thread.start()
+schedule_thread.start()
+request_thread.join()
+workers_thread.join()
+schedule_thread.join()
--- YAC/worker.py
+++ YAC/worker.py
@@ -19,167 +19,176 @@ class Worker:
     def __init__(self, port, worker_id):
         self.port = port
         self.worker_id = worker_id
-        self.exec_pool = list()
         self.free_slots = 0
-        
-        self.slot_count = worker_id-1
-        self.slots = slots[self.slot_count]
-        self.free_slots = slots[self.slot_count]
-        self.exec_pool = [0 for x in range(slots[self.slot_count])]
+        try:
+            self.slots = slots[self.slot_count]
+        except:
+            self.slots = 0
+        self.free_slots = self.slots
+        self.exec_pool = [0 for x in range(self.slots)]
         
         try: 
             f = open(f"log/worker_{w_id}.txt", "a+")
-            printLock.acquire()
+            log_lock.acquire()
             print(datetime.now().strftime("%m/%d/%Y, %H:%M:%S.%f") + f":\tWORKER HAS STARTED = [worker_id:{self.worker_id}, port:{self.port}, slots:{self.slots}]",file=f)
-            printLock.release()
-        finally:
+            log_lock.release()
+        except:
+            print("UNKNOWN ERROR OCCURED DURING LOGGING")
+        else:
             f.close()
 
-    
-    #get the task message and lauch the task if the there is a free slot
     def task_launch(self, task_dict):
         task = Task(task_dict["job_id"], task_dict["task_id"], task_dict["duration"])
-        
         try :
             f = open(f"log/worker_{w_id}.txt", "a+")
-            printLock.acquire()
+            log_lock.acquire()
             print(datetime.now().strftime("%m/%d/%Y, %H:%M:%S.%f") + f":\tTASK STARTED = [job_id:{task_dict['job_id']}, task_id:{task_dict['task_id']}]",file=f)
-            printLock.release()
-        finally:
+            log_lock.release()
+        except:
+            print("UNKNOWN ERROR OCCURED DURING LOGGING")
+        else:
             f.close()
         
-        limit = len(self.exec_pool)
+        limit = self.slots
         for i in range(limit):
-            if (not isinstance(self.exec_pool[i], int) or self.exec_pool[i] != 0):
+            if not(type(self.exec_pool[i])==type(1) and self.exec_pool[i] == 0):
                 pass
             else:
                 self.exec_pool[i] = task
                 self.free_slots = self.free_slots - 1
                 break
             
-    
-   # When time is 0, remove the task form the exectuion pool 
     def remove_task(self, exec_pool_index):
         self.exec_pool[exec_pool_index] = 0
         self.free_slots = self.free_slots + 1
 
 #Task def
 class Task:
-    #set the values up for the job, task and time remaining
     def __init__(self, job_id, task_id, time_left):
         self.time_left = time_left
         self.task_id = task_id
         self.job_id = job_id
-       
-    
-    #decrement by 1 
+
     def reduce(self):
         self.time_left = self.time_left - 1
-        
-    #is there time left in the task 
-    def check_time_remaining(self):
-        time = self.time_left
-        return bool(time)
 
 
 
-
-#1st thread; listen for the "task launch" message and then adds the task to the execution pool
 def get_task_launch_msg(worker):
-    s = socket(AF_INET, SOCK_STREAM)
-    with s:
+    try:
+        s = socket(AF_INET, SOCK_STREAM)
         s.bind(("localhost", worker.port))
         s.listen(1024)
         for i in iter(int,1):
             c, addr = s.accept()
-            
             try:
                 f = open(f"log/worker_{w_id}.txt", "a+")
-                printLock.acquire()
+                log_lock.acquire()
                 print(datetime.now().strftime("%m/%d/%Y, %H:%M:%S.%f") + f":\tCONNECTION INCOMING = [host:{addr[0]}, port:{addr[1]}]",file=f)
-                printLock.release()
-            finally:
+                log_lock.release()
+            except:
+                print("UNKNOWN ERROR OCCURED DURING LOGGING")
+            else:
                 f.close()
             with c:
                 task_launch_msg_json = c.recv(1024).decode()
                 if not task_launch_msg_json:
-                    pass
+                    continue
                 else:
                     task = json.loads(task_launch_msg_json)
-                    
                     try:
                         f = open(f"log/worker_{w_id}.txt", "a+")
-                        printLock.acquire()
+                        log_lock.acquire()
                         print(datetime.now().strftime("%m/%d/%Y, %H:%M:%S.%f") + f":\tTASK RECEIVED = [job_id:{task['job_id']}, task_id:{task['task_id']}, duration:{task['duration']}]",file=f)
-                        printLock.release()
-                    finally:
+                        log_lock.release()
+                    except:
+                        print("UNKNOWN ERROR OCCURED DURING LOGGING")
+                    else:
                         f.close()
-                    workerLock.acquire()
+                    workers_lock.acquire()
                     worker.task_launch(task)
-                    workerLock.release()
-        
-        
+                    workers_lock.release()
+    except Exception as e:
+        try:
+            f = open("log/master.txt", "a+")
+            log_lock.acquire()
+            print(datetime.now().strftime("%m/%d/%Y, %H:%M:%S.%f") + f"\tERROR OCCURED [error_message: {e}]",file=f)
+            log_lock.release()
+        except:
+            print("UNKNOWN ERROR OCCURED DURING LOGGING")
+        else:
+            f.close()
+    else:
+        s.close()
+
 #4th thread goes through each task and reduce the time for each task       
 def execution_of_tasks(worker):
     for i in iter(int,1):
-        #if there are workers 
-        if(worker.slots == 0):
+        if(not(worker.slots)):
         	continue    
         for i in range(worker.slots):
-            if (isinstance(worker.exec_pool[i], int) and worker.exec_pool[i] == 0):
+            if (type(worker.exec_pool[i])==type(1)) and not(worker.exec_pool[i]):
                 continue
-            elif (worker.exec_pool[i].check_time_remaining()):
+            elif (worker.exec_pool[i].time_left>0):
                 task = worker.exec_pool[i]
                 job_id = task.job_id
                 task_id = task.task_id
-                msg = { 
-                    "worker_id": worker.worker_id, 
-                    "job_id": job_id, 
-                    "task_id": task_id
-                    }
-                    
+                msg = {"worker_id": worker.worker_id,"job_id": job_id, "task_id": task_id}
                 try:
                     f =  open(f"log/worker_{w_id}.txt", "a+")
-                    printLock.acquire()
+                    log_lock.acquire()
                     print(datetime.now().strftime("%m/%d/%Y, %H:%M:%S.%f") + f":\tTASK FINISHED = [job_id:{job_id}, task_id:{task_id}]",file=f)
-                    printLock.release()
-                finally:
+                    log_lock.release()
+                except:
+                    print("UNKNOWN ERROR OCCURED DURING LOGGING")
+                else:
                     f.close()
-
-                workerLock.acquire()
+                workers_lock.acquire()
                 worker.remove_task(i)
-                workerLock.release()
+                workers_lock.release()
                 
                 msg_str = json.dumps(msg)
                 s = socket(AF_INET, SOCK_STREAM)
-                with s:
+                try:
                     s.connect(('localhost', 5001))
                     f = open(f"log/worker_{w_id}.txt", "a+")
-                    #with open(f"log/worker_{w_id}.txt", "a+") as f:
                     try:
-                        printLock.acquire()
+                        log_lock.acquire()
                         print(datetime.now().strftime("%m/%d/%Y, %H:%M:%S.%f") + f":\tCONNECTION OUTGOING = [host:{'localhost'}, port:{5001}]",file=f)
-                        printLock.release()
-                    finally:
+                        log_lock.release()
+                    except:
+                        print("UNKNOWN ERROR OCCURED DURING LOGGING")
+                    else:
                         f.close()
                     s.send(msg_str.encode())
-                #with open(f"log/worker_{w_id}.txt", "a+") as f:
+                except Exception as e:
+                    try:
+                        f = open("log/master.txt", "a+")
+                        log_lock.acquire()
+                        print(datetime.now().strftime("%m/%d/%Y, %H:%M:%S.%f") + f"\tERROR OCCURED [error_message: {e}]",file=f)
+                        log_lock.release()
+                    except:
+                        print("UNKNOWN ERROR OCCURED DURING LOGGING")
+                    else:
+                        f.close()
                 try:
                     f = open(f"log/worker_{w_id}.txt", "a+")
-                    printLock.acquire()
+                    log_lock.acquire()
                     print(datetime.now().strftime("%m/%d/%Y, %H:%M:%S.%f") + f":\tUPDATE SENT TO MASTER = [job_id:{job_id}, task_id:{task_id}] COMPLETED",file=f)
-                    printLock.release()
-                finally:
+                    log_lock.release()
+                except:
+                    print("UNKNOWN ERROR OCCURED DURING LOGGING")
+                else:
                     f.close()
             else:
-                workerLock.acquire()
+                workers_lock.acquire()
                 worker.exec_pool[i].reduce()
-                workerLock.release()
+                workers_lock.release()
     sleep(1)
   
-taskLock = threading.Lock()
-workerLock = threading.Lock()
-printLock = threading.Lock() 
+tasks_lock = threading.Lock()
+workers_lock = threading.Lock()
+log_lock = threading.Lock() 
 
 
 try:
@@ -189,8 +198,8 @@ except:
 open(f"log/worker_{w_id}.txt", "w").close()
 if (len(sys.argv) != 3):
     print("Usage: python Worker.py <port number> <worker id>")
-worker = Worker(int(sys.argv[1],int(sys.argv[2]))
 
+worker = Worker(int(sys.argv[1]),int(sys.argv[2]))
 t1 = threading.Thread(target = get_task_launch_msg, args = (worker,))
 t2 = threading.Thread(target = execution_of_tasks, args = (worker,))
 t1.start()
